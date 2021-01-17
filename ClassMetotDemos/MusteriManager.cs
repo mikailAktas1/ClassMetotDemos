7dd1a99e821b99cc76476acd06d5bbc4d638367b
mikail aktas <mikail.aktas1@hotmail.com>
2021-01-17 19:13:54 +0300

    Add project files.

---
+++ ClassMetotDemos/MusteriManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemos
+{
+    class MusteriManager
+    {
+
+        Musteri musteri = new Musteri();
+
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri eklendi." + " " + musteri.Adi);
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri silindi.");
+        }
+        public void Listele(Musteri[] musteriler)
+        {
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Id + " " + musteri.Adi + " " + musteri.Soyadi);
+            }
+        }
+    }
+}
